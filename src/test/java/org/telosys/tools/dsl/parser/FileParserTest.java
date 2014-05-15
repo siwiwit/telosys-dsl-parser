d159d36e284c4b4374536be7b5747d8adf95c672
jonag <jonag@jonag.org>
2014-05-15 15:41:26 +0200

    Adding test parsing rules

--- src/test/java/org/telosys/tools/dsl/parser/FileParserTest.java
+++ src/test/java/org/telosys/tools/dsl/parser/FileParserTest.java
@@ -28,4 +28,54 @@ public class FileParserTest extends TestCase {
         String file = fileParser.checkSyntax(fileParser.readStream(inputStream), "Employee");
         System.out.println(file);
     }
+
+    @Test
+    public void testParsingRules() {
+        String testValid = "Entity{id:int;}";
+        String testMissingBracket = "Entityid:int;";
+        String testMultipleEntities = "Entity{id:int;}Entity2{id:int;}";
+        String testEntityWithoutField = "Entity{}";
+        String testEntityNameIllegalCharacters = "E#n_tité{id:int;}";
+        String testEntityFieldIllegalCharacters = "Entity{ié#_:int;}";
+        String testMissingSemiColumn = "Entity{id:int;name:string}";
+        String testMissingLastBracket = "Entity{id:int;";
+
+        FileParser fileParser = new FileParser();
+        fileParser.checkSyntax(testValid, "Entity");
+
+        try {
+            fileParser.checkSyntax(testMissingBracket, "Entity");
+            fail("Brackets are missing");
+        } catch (EntityParserException e) {}
+
+        try {
+            fileParser.checkSyntax(testMultipleEntities, "Entity");
+            fail("Multiple entities are defined");
+        } catch (EntityParserException e) {}
+
+        try {
+            fileParser.checkSyntax(testEntityWithoutField, "Entity");
+            fail("No field are defined");
+        } catch (EntityParserException e) {}
+
+        try {
+            fileParser.checkSyntax(testEntityNameIllegalCharacters, "E#n_tité");
+            fail("Entity name contains illegal characters");
+        } catch (EntityParserException e) {}
+
+        try {
+            fileParser.checkSyntax(testEntityFieldIllegalCharacters, "Entity");
+            fail("Entity field contains illegal characters");
+        } catch (EntityParserException e) {}
+
+        try {
+            fileParser.checkSyntax(testMissingSemiColumn, "Entity");
+            fail("Last semicolumn is missing");
+        } catch (EntityParserException e) {}
+
+        try {
+            fileParser.checkSyntax(testMissingLastBracket, "Entity");
+            fail("Last bracket is missing");
+        } catch (EntityParserException e) {}
+    }
 }
