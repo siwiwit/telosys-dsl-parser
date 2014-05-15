47d98292753b72b15c90dd676aa52ccb71d3221f
jonag <jonag@jonag.org>
2014-05-15 13:40:28 +0200

    Typo

--- src/main/java/org/telosys/tools/dsl/parser/EntityParser.java
+++ src/main/java/org/telosys/tools/dsl/parser/EntityParser.java
@@ -8,7 +8,7 @@ import java.io.InputStream;
  * 
  * First entry point for the telosys entity parser
  * 
- * @author Jonathan Goncalvez, Mathieu Herbert, Thomas Legendre
+ * @author Jonathan Goncalves, Mathieu Herbert, Thomas Legendre
  * @date 2014-05-13
  * @version 1.0
  */
