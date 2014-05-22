a5470953169a6e6a93e5057a4f1bedc7e5a1028a
jonag <jonag@jonag.org>
2014-05-22 16:18:03 +0200

    Adding tests

--- src/test/java/org/telosys/tools/dsl/parser/EntityParserTest.java
+++ src/test/java/org/telosys/tools/dsl/parser/EntityParserTest.java
@@ -1,12 +1,12 @@
 package org.telosys.tools.dsl.parser;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.telosys.tools.dsl.parser.model.Entity;
+import org.telosys.tools.dsl.parser.model.Field;
+
+import java.io.File;
 
 public class EntityParserTest {
 	@Before
@@ -27,12 +27,93 @@ public class EntityParserTest {
                 "\tid : id {@test}; // the id\n" +
                 "\tfirstName : string ;\n" +
                 "\tbirthDate : date ;\n" +
-                "}";
+                "}\n";
 
         EntityParser parser = new EntityParser(formattedContent);
         String flattenContent = parser.computeFlattenContent();
 
         String compareTo = "Employee{id:id{@test};firstName:string;birthDate:date;}";
         Assert.assertEquals(compareTo, flattenContent);
+    }
+
+    @Test(expected = EntityParserException.class)
+    public void testParseMissingBracket() {
+        String testMissingBracket = "Entityid:int;";
+
+        EntityParser parser = new EntityParser();
+        parser.setFlattenContent(testMissingBracket);
+        parser.parseFlattenContent("Entity");
+    }
+
+    @Test(expected = EntityParserException.class)
+    public void testParseMultipleEntities() {
+        String testMultipleEntities = "Entity{id:int;}Entity2{id:int;}";
+
+        EntityParser parser = new EntityParser();
+        parser.setFlattenContent(testMultipleEntities);
+        parser.parseFlattenContent("Entity");
+    }
+
+    @Test(expected = EntityParserException.class)
+    public void testParseWithoutField() {
+        String testEntityWithoutField = "Entity{}";
+
+        EntityParser parser = new EntityParser();
+        parser.setFlattenContent(testEntityWithoutField);
+        parser.parseFlattenContent("Entity");
+    }
+
+    @Test(expected = EntityParserException.class)
+    public void testParseEntityWithIllegalCharacters() {
+        String testEntityNameIllegalCharacters = "E#n_tité{id:int;}";
+
+        EntityParser parser = new EntityParser();
+        parser.setFlattenContent(testEntityNameIllegalCharacters);
+        parser.parseFlattenContent("Entity");
+    }
+
+    @Test(expected = EntityParserException.class)
+    public void testParseFieldWithIllegalCharacters() {
+        String testEntityFieldIllegalCharacters = "Entity{ié#_:int;}";
+
+        EntityParser parser = new EntityParser();
+        parser.setFlattenContent(testEntityFieldIllegalCharacters);
+        parser.parseFlattenContent("Entity");
+    }
+
+    @Test(expected = EntityParserException.class)
+    public void testParseMissingSemiColumn() {
+        String testMissingSemiColumn = "Entity{id:int;name:string}";
+
+        EntityParser parser = new EntityParser();
+        parser.setFlattenContent(testMissingSemiColumn);
+        parser.parseFlattenContent("Entity");
+    }
+
+    @Test(expected = EntityParserException.class)
+    public void testParseMissingLastBracket() {
+        String testMissingLastBracket = "Entity{id:int;";
+
+        EntityParser parser = new EntityParser();
+        parser.setFlattenContent(testMissingLastBracket);
+        parser.parseFlattenContent("Entity");
+    }
+
+    @Test()
+    public void testParseValid() {
+        String testValid = "Entity{id:int;}";
+
+        EntityParser parser = new EntityParser();
+        parser.setFlattenContent(testValid);
+        Entity entity = parser.parseFlattenContent("Entity");
+
+        Entity toCompare = new Entity("Entity");
+        Field fieldId = new Field("id", "id:int");
+        toCompare.addField(fieldId);
+
+        System.out.println(entity);
+        System.out.println(toCompare);
+
+        Assert.assertEquals(toCompare, entity);
     }
 }
---
+++ src/test/java/org/telosys/tools/dsl/parser/utils/StringUtilsTest.java
@@ -0,0 +1,26 @@
+package org.telosys.tools.dsl.parser.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class StringUtilsTest {
+    @Test
+    public void testReadStream() throws Exception {
+        File file = new File("entity_test/Employee.entity");
+        InputStream inputStream = new FileInputStream(file);
+
+        String formattedContent = StringUtils.readStream(inputStream);
+
+        String compareTo = "Employee {\n" +
+                "\tid : id {@test}; // the id\n" +
+                "\tfirstName : string ;\n" +
+                "\tbirthDate : date ;\n" +
+                "}\n";
+
+        Assert.assertEquals(compareTo, formattedContent);
+    }
+}
