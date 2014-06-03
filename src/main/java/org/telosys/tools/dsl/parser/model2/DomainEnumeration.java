dfc5578ced73e234ab0d28f107aca023049e4c58
math.herbert <math.herbert@gmail.com>
2014-06-03 22:27:33 +0200

    add equals en hashcode methods

--- src/main/java/org/telosys/tools/dsl/parser/model2/DomainEnumeration.java
+++ src/main/java/org/telosys/tools/dsl/parser/model2/DomainEnumeration.java
@@ -58,4 +58,29 @@ public abstract class DomainEnumeration<T> extends DomainType {
         return list;
     }
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((items == null) ? 0 : items.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DomainEnumeration<T> other = (DomainEnumeration<T>) obj;
+		if (items == null) {
+			if (other.items != null)
+				return false;
+		} else if (!items.equals(other.items))
+			return false;
+		return true;
+	}
+
 }
--- src/main/java/org/telosys/tools/dsl/parser/model2/DomainEnumerationItem.java
+++ src/main/java/org/telosys/tools/dsl/parser/model2/DomainEnumerationItem.java
@@ -2,6 +2,37 @@ package org.telosys.tools.dsl.parser.model2;
 
 public class DomainEnumerationItem<T> {
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DomainEnumerationItem<T> other = (DomainEnumerationItem<T>) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
 	private final String name ;
 	
 	private final T value ;
