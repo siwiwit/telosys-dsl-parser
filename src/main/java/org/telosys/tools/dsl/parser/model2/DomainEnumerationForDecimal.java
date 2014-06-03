02955994e22817743b28bdc67d35b9e27b707b9e
laguerin <laguerin@FR-44-02-11-017.corp.capgemini.com>
2014-06-03 19:12:48 +0200

    Enumeration for Decimal

---
+++ src/main/java/org/telosys/tools/dsl/parser/model2/DomainEnumerationForDecimal.java
@@ -0,0 +1,37 @@
+package org.telosys.tools.dsl.parser.model2;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @author Jonathan Goncalves, Mathieu Herbert, Thomas Legendre
+ * @date 2014-05-13
+ * @version 1.0
+ */
+public class DomainEnumerationForDecimal extends DomainEnumeration<BigDecimal> {
+
+    public DomainEnumerationForDecimal(String name) {
+    	super(name);
+    }
+
+    public void addItem( String name, BigDecimal value) {
+        super.addItem(name, value);
+    }
+
+    public void addItem(DomainEnumerationItem<BigDecimal> item) {
+        super.addItem(item);
+    }
+
+    public DomainEnumerationItem<BigDecimal> getItem(String name) {
+    	return super.getItem(name);
+    }
+
+    public BigDecimal getItemValue(String name) {
+    	return super.getItemValue(name);
+    }
+
+    public List<DomainEnumerationItem<BigDecimal>> getItems() {
+        return super.getItems();
+    }
+
+}
---
+++ src/test/java/org/telosys/tools/dsl/parser/model/DomainEnumerationForDecimalTest.java
@@ -0,0 +1,46 @@
+package org.telosys.tools.dsl.parser.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+import org.telosys.tools.dsl.parser.EntityParserException;
+import org.telosys.tools.dsl.parser.model2.DomainEnumerationForDecimal;
+
+public class DomainEnumerationForDecimalTest {
+
+	@Test
+	public void test() {
+		DomainEnumerationForDecimal en = new DomainEnumerationForDecimal("Values");
+		
+		assertTrue ( en.isEnumeration() );
+		assertFalse ( en.isEntity() );
+		assertFalse ( en.isNeutralType() );
+		assertTrue( en.getNumberOfItems() == 0 ) ;
+		
+		en.addItem("V1", new BigDecimal("10.50") );
+		assertTrue( en.getNumberOfItems() == 1 ) ;
+		
+		en.addItem("V2", new BigDecimal("222") );
+		assertTrue( en.getNumberOfItems() == 2 ) ;
+		
+		assertEquals( en.getItemValue("V1"), new BigDecimal( "10.50") ) ;
+		assertEquals( en.getItemValue("V2"), new BigDecimal("222") ) ;
+		
+		BigDecimal v1 = en.getItemValue("V1");
+		BigDecimal v2 = en.getItemValue("V2");
+		v1.add(v2);
+		assertEquals( v1.add(v2), new BigDecimal("232.50") ) ;
+	}
+	
+	@Test ( expected = EntityParserException.class )
+	public void testItemDuplicated1() {
+		DomainEnumerationForDecimal en = new DomainEnumerationForDecimal("Values");
+		en.addItem("V1", new BigDecimal("10.50") );
+		en.addItem("V1", new BigDecimal("11") );
+	}
+
+}
