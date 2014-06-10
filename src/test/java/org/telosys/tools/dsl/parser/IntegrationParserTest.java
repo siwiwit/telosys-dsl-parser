62d297eae3c2bdff3011c412ed32dc97d3fba040
math.herbert <math.herbert@gmail.com>
2014-06-10 13:52:53 +0200

    modify test

--- src/test/java/org/telosys/tools/dsl/parser/IntegrationParserTest.java
+++ src/test/java/org/telosys/tools/dsl/parser/IntegrationParserTest.java
@@ -1,26 +1,14 @@
 package org.telosys.tools.dsl.parser;
 
-import junit.framework.Assert;
+import java.io.File;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.telosys.tools.dsl.parser.model.DomainModel;
-
-import java.io.File;
 
 
 public class IntegrationParserTest {
 
-
-    @Test
-    public void testParseValidFiles() throws Exception {
-        File folder = new File("src/test/resources/entity_test/valid");
-        for (final File fileEntry : folder.listFiles()) {
-            if (!fileEntry.isDirectory()) {
-            	EntityParser parser = new EntityParser(new DomainModel("model"));
-                parser.parse(fileEntry);
-            }
-        }
-    }
 
     @Test
     public void testParseInvalidFiles() throws Exception {
