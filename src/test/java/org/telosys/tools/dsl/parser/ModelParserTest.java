d84f3aa6e779d14b84c5dff85e5acd414291d3eb
math.herbert <math.herbert@gmail.com>
2014-06-10 14:36:34 +0200

    add test in ModelParser

--- src/test/java/org/telosys/tools/dsl/parser/ModelParserTest.java
+++ src/test/java/org/telosys/tools/dsl/parser/ModelParserTest.java
@@ -9,6 +9,9 @@ import org.junit.Test;
 import org.telosys.tools.dsl.parser.model.DomainEntity;
 import org.telosys.tools.dsl.parser.model.DomainEntityField;
 import org.telosys.tools.dsl.parser.model.DomainEntityFieldAnnotation;
+import org.telosys.tools.dsl.parser.model.DomainEnumeration;
+import org.telosys.tools.dsl.parser.model.DomainEnumerationForString;
+import org.telosys.tools.dsl.parser.model.DomainEnumerationItem;
 import org.telosys.tools.dsl.parser.model.DomainModel;
 import org.telosys.tools.dsl.parser.model.DomainNeutralTypes;
 
@@ -59,6 +62,47 @@ public class ModelParserTest {
 		countryField.addAnnotation(new DomainEntityFieldAnnotation("Max", "3"));
 		employee.addField(countryField);
 		modelToCompare.addEntity(employee);
+		assertEquals(modelToCompare,model);
+	}
+	
+	@Test
+	public void testParseModelWithAnEnum() throws Exception {
+		File folder = new File(
+				"src/test/resources/model_test/valid/model_WithAnEnum");
+		DomainModelParser parser = new DomainModelParser();
+		DomainModel model = parser.parse(folder);
+		DomainModel modelToCompare = new DomainModel("ModelWithAnEnum");
+		DomainEnumeration<String> enumeration = new DomainEnumerationForString("Pays");
+		enumeration.addItem(new DomainEnumerationItem<String>("FR", "France"));
+		enumeration.addItem(new DomainEnumerationItem<String>("EN", "Angleterre"));
+		enumeration.addItem(new DomainEnumerationItem<String>("ES", "Espagne"));
+		modelToCompare.addEnumeration(enumeration);
+		assertEquals(modelToCompare,model);
+	}
+	
+	
+	@Test
+	public void testParseModelWithAnEnumAndAnEntity() throws Exception {
+		File folder = new File(
+				"src/test/resources/model_test/valid/model_WithAnEnumAndAnEntity");
+		DomainModelParser parser = new DomainModelParser();
+		DomainModel model = parser.parse(folder);
+		DomainModel modelToCompare = new DomainModel("ModelWithAnEnumAndAnEntity");
+		DomainEnumeration<String> enumeration = new DomainEnumerationForString("Pays");
+		enumeration.addItem(new DomainEnumerationItem<String>("FR", "France"));
+		enumeration.addItem(new DomainEnumerationItem<String>("EN", "Angleterre"));
+		enumeration.addItem(new DomainEnumerationItem<String>("ES", "Espagne"));
+		
+		DomainEntity employee = new DomainEntity("Employee");
+		DomainEntityField id = new DomainEntityField("id", DomainNeutralTypes.getType("integer"));
+		id.addAnnotation(new DomainEntityFieldAnnotation("Id"));
+		employee.addField(id);
+		employee.addField(new DomainEntityField("firstName", DomainNeutralTypes.getType("string")));
+		employee.addField(new DomainEntityField("birthDate", DomainNeutralTypes.getType("date")));
+		DomainEntityField countryField = new DomainEntityField("country", enumeration );
+		employee.addField(countryField);
+		modelToCompare.addEntity(employee);
+		modelToCompare.addEnumeration(enumeration);
 		assertEquals(modelToCompare,model);
 	}
 }
