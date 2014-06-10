f984717aab8cf3aa0699361293e67b607202c0fe
math.herbert <math.herbert@gmail.com>
2014-06-10 14:22:02 +0200

    add a test in ModelParser

--- src/test/java/org/telosys/tools/dsl/parser/ModelParserTest.java
+++ src/test/java/org/telosys/tools/dsl/parser/ModelParserTest.java
@@ -32,10 +32,33 @@ public class ModelParserTest {
 		employee.addField(new DomainEntityField("firstName", DomainNeutralTypes.getType("string")));
 		employee.addField(new DomainEntityField("birthDate", DomainNeutralTypes.getType("date")));
 		modelToCompare.addEntity(employee);
-		System.out.println(model);
-		System.out.println(modelToCompare);
 		assertEquals(modelToCompare,model);
+	}
+	
+	@Test
+	public void testParseModelWithTwoEntities() throws Exception {
+		File folder = new File(
+				"src/test/resources/model_test/valid/model_WithTwoEntities");
+		DomainModelParser parser = new DomainModelParser();
+		DomainModel model = parser.parse(folder);
+		DomainModel modelToCompare = new DomainModel("ModelWithTwoEntities");
+		DomainEntity country = new DomainEntity("Country");
+		DomainEntityField idCountry = new DomainEntityField("id", DomainNeutralTypes.getType("integer"));
+		idCountry.addAnnotation(new DomainEntityFieldAnnotation("Id"));
+		country.addField(idCountry);
+		country.addField(new DomainEntityField("name", DomainNeutralTypes.getType("string")));
+		modelToCompare.addEntity(country);
 		
-
+		DomainEntity employee = new DomainEntity("Employee");
+		DomainEntityField id = new DomainEntityField("id", DomainNeutralTypes.getType("integer"));
+		id.addAnnotation(new DomainEntityFieldAnnotation("Id"));
+		employee.addField(id);
+		employee.addField(new DomainEntityField("firstName", DomainNeutralTypes.getType("string")));
+		employee.addField(new DomainEntityField("birthDate", DomainNeutralTypes.getType("date")));
+		DomainEntityField countryField = new DomainEntityField("country", country);
+		countryField.addAnnotation(new DomainEntityFieldAnnotation("Max", "3"));
+		employee.addField(countryField);
+		modelToCompare.addEntity(employee);
+		assertEquals(modelToCompare,model);
 	}
 }
